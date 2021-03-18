f5d2d47ff62e39f3e8303bb7fcc98d040a37a7df
haobing.wang <9262286+uwang@users.noreply.github.com>
2021-03-18 18:08:23 +0800

    Update set_mirror.sh

--- set_mirror.sh
+++ set_mirror.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 apt install apt-transport-https ca-certificates -y
 
@@ -7,16 +8,14 @@ cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
 
 cat>/etc/apt/sources.list<<EOF
 deb https://mirrors.ustc.edu.cn/debian/ buster main contrib non-free
-deb-src https://mirrors.ustc.edu.cn/debian/ buster main contrib non-free
-
+# deb-src http://mirrors.ustc.edu.cn/debian buster main contrib non-free
 deb https://mirrors.ustc.edu.cn/debian/ buster-updates main contrib non-free
-deb-src https://mirrors.ustc.edu.cn/debian/ buster-updates main contrib non-free
-
-deb https://mirrors.ustc.edu.cn/debian-security/ buster/updates main contrib non-free
-deb-src https://mirrors.ustc.edu.cn/debian-security/ buster/updates main contrib non-free
+# deb-src http://mirrors.ustc.edu.cn/debian buster-updates main contrib non-free
+deb https://mirrors.ustc.edu.cn/debian-security buster/updates main contrib non-free
+# deb-src http://mirrors.ustc.edu.cn/debian-security/ buster/updates main non-free contrib
 EOF
 
-cat>/etc/apt/sources.list<<EOF
+cat>/etc/apt/sources.list.d/raspi.list<<EOF
 deb http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ buster main ui
 #deb-src http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ buster main ui
 EOF
