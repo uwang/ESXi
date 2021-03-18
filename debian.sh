7c43aea247a55265942e727e80a7f19d3cc2e920
haobing.wang <9262286+uwang@users.noreply.github.com>
2021-03-18 18:14:22 +0800

    Create debian.sh

---
+++ debian.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+apt install apt-transport-https ca-certificates -y
+
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cp /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
+
+cat>/etc/apt/sources.list<<EOF
+deb https://mirrors.ustc.edu.cn/debian/ buster main contrib non-free
+deb-src https://mirrors.ustc.edu.cn/debian/ buster main contrib non-free
+
+deb https://mirrors.ustc.edu.cn/debian/ buster-updates main contrib non-free
+deb-src https://mirrors.ustc.edu.cn/debian/ buster-updates main contrib non-free
+
+deb https://mirrors.ustc.edu.cn/debian-security/ buster/updates main contrib non-free
+deb-src https://mirrors.ustc.edu.cn/debian-security/ buster/updates main contrib non-free
+EOF
+
+cat>/etc/apt/sources.list.d/raspi.list<<EOF
+deb http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ buster main ui
+#deb-src http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ buster main ui
+EOF
+
+apt-get update
